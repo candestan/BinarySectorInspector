0b7305cc7ce7a6073c8ed9eb0dfdc2981b8e97ea
septillioner <egeismailkosedag@gmail.com>
2026-08-14 17:27:19 +0300

    Remove deprecated PE analysis and application files, including app.cpp, app.h, and related PE components. This cleanup eliminates unused code and prepares for a more streamlined architecture.

--- ege_pending/platform/file_picker.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <windows.h>
-#include <string>
-
-bool pick_pe_file(HWND owner, std::wstring& out_path);
--- ege_pending/platform/utf8.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-#include <string_view>
-
-std::string wide_to_utf8(std::wstring_view wide);
